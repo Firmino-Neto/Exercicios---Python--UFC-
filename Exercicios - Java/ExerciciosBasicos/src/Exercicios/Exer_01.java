9ae9276d7aeb5c8edb3e40837aef3ae213981bd4
João Firmino <73793246+Firmino-Neto@users.noreply.github.com>
2020-11-25 15:45:00 -0300

    Add Exercicios JAVA

---
+++ Exercicios - Java/ExerciciosBasicos/src/Exercicios/Exer_01.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ */
+package Exercicios;
+
+/**
+ *
+ * @author joaon
+ */
+public class Exer_01 {
+    public static void main(String[] args){
+            System.out.println("Hello world");
+    }
+    
+}
